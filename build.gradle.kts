66c478b97051d15dc81eb65f9f8465ba8699ec2f
zxc <44724385@qq.com>
2021-06-25 15:44:37 +0800

    add java-lib plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    `java-library`
 }
 
 group = "com.oasis.mlib"
@@ -10,6 +11,7 @@ version = "v0.0.1"
 repositories {
     mavenCentral()
     mavenLocal()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
