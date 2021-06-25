e71539fa6a683c19435ea92674bcb8262ecd49de
zxc <44724385@qq.com>
2021-06-25 15:51:33 +0800

    add publish plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.10"
     `java-library`
+    `maven-publish`
 }
 
 group = "com.oasis.mlib"
@@ -25,4 +26,38 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("KotlinUtils") {
+            artifactId = "KotlinUtils"
+            from(components["java"])
+            pom {
+                name.set("Kotlin Utils")
+                description.set("oasis mlib kotlin utils library")
+                url.set("https://github.com/oasmobile/kotlin-utils")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("zhangxuchang")
+                        name.set("ZHANG XUCHANG")
+                        email.set("zhangxuchang@oasgames.com")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "KotlinUtils"
+            url = uri(layout.buildDirectory.dir("repo"))
+        }
+    }
 }
